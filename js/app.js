7e89b15ed9a0008e14fd04f4ef0d3b54d3ad6c08
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-06-04 13:16:13 +0530

    Change in mobile navigation

--- js/app.js
+++ js/app.js
@@ -5,14 +5,12 @@ const introContainer = document.querySelector(".intro-container");
 const navLinksLI = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("nav-active");
-  toggleNav.src = "../images/icon-close.svg";
+  navLinks.classList.toggle("open");
 
-  if (navLinks.classList.contains("nav-active")) {
-    navLinks.style.visibility = "visible";
+  if (navLinks.classList.contains("open")) {
+    toggleNav.src = "../images/icon-close.svg";
     introContainer.style.backgroundColor = "rgba(0,0,0,0.3)";
   } else {
-    navLinks.style.visibility = "hidden";
     toggleNav.src = "../images/icon-hamburger.svg";
     introContainer.style.backgroundColor = "rgba(0,0,0,0)";
   }
